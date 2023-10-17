7eac43d70b1588db90ec32d708cfd61c4dd9cfaa
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2023-10-17 03:16:49 +0000

    rock

--- app.rb
+++ app.rb
@@ -7,3 +7,30 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  comp_move = moves.sample
+  
+  if comp_move == "rock"
+    outcome = "tied"
+  elsif comp_move == "scissors"
+    outcome = "won"
+  else
+    outcome = "lost"
+  end
+
+"
+  <h2>
+    We played rock!
+  </h2>
+
+  <h2>
+   They played #{comp_move}!
+  </h2>
+
+  <h2>
+   We tied!
+  </h2>
+  "
+end
